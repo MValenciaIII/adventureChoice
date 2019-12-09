cd9e5566fde5b5bcebfefdcc5ac269b2cbe8a898
Margarito Valencia <margaritovalenciaIII@gmail.com>
2019-12-09 16:17:08 -0600

    added functions to characters

--- js/app.js
+++ js/app.js
@@ -5,19 +5,53 @@ const windowConfirm = ()=> {
 }
 windowConfirm ();
 
+function yourself(text) {
+    let msg = document.createElement('p'); //create a function with these 3 msges
+    msg.style.color = 'gray';
+    textBox.append(msg); 
+    msg.innerHTML += text
+}
+
+function rolaf(text) {
+    let msg = document.createElement('p'); //create a function with these 3 msges
+    msg.style.color = 'rgb(116, 2, 2);'; // class, color,
+    textBox.append(msg); 
+    msg.innerHTML += text
+}
+
 
 var textBox = document.getElementById('screen');
 var userText = document.getElementById('userText');
 var userAnswer = document.getElementById('answerBox');
+var answerStore = '';
+
+
+
 const textConsole = ()=> {
     setTimeout(() => {
-        textBox.innerHTML = 'Ralof: Hey, you. You\'re finally awake.' 
+        rolaf('Guy: Hey, you. You\'re finally awake.'); 
         setTimeout(() => {
-            userText.innerHTML += 'Myself: Am i awake? (Yes/No) '    
+            yourself('Myself: Am i awake? (Yes/No)');
         }, 2000);
     }, 3000);
-    
-    
+    //userAnswer = answerStore
+    userAnswer.addEventListener('blur', (event) => {
+
+        if (userAnswer.value == 'Yes') {
+            yourself(' Myself: Yes, I am fully awake. Where am i? ');
+        } 
+            
+        else {
+             yourself(' Myself: No. *closes eyes* ');
+            setTimeout(() => {
+                rolaf('Guy: ...');
+                setTimeout(() => {
+                    rolaf('Guy: You jusy answered me..');
+                }, 2000);
+            }, 2000);
+        }
+
+    })
 }
 
 textConsole();
